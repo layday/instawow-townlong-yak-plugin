4e3e968faaefca7ac91ca4e9d928a05d7102a993
layday <layday@protonmail.com>
2021-02-03 01:54:50 +0200

    Update to use the WowUp wrapper API

--- instawow_townlong_yak_plugin.py
+++ instawow_townlong_yak_plugin.py
@@ -4,70 +4,104 @@ from __future__ import annotations
 
 from datetime import datetime, timezone
 
+from instawow.manager import cache_response
 from instawow.models import Pkg, PkgOptions
 import instawow.plugins
 from instawow.resolvers import Defn, Strategy, Resolver
-from instawow.results import PkgNonexistent, PkgStrategyUnsupported
+from instawow.results import PkgNonexistent, PkgStrategyUnsupported, PkgFileUnavailable
 from typing_extensions import Literal, TypedDict
 from yarl import URL
 
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 
-class TownlongYakApiResponse_Release(TypedDict):
-    ch: Literal[0, 1]  # Channel: 0 = retail; 1 = classic
-    cv: list[str]  # Compatible game versions, e.g. "9.0.2"
-    dl: str  # File download URL
+class WowUpHubAddon(TypedDict):
+    id: int
+    repository: str  # Home page URL
+    repository_name: str  # Add-on name
+    source: str
+    description: str  # Includes HTML
+    homepage: str  # Source URL
+    image_url: str
+    owner_image_url: str
+    owner_name: str  # Author
+    total_download_count: int  # Always 0 for Townlong Yak
+    funding_links: list[str]
+    releases: list[WowUpHubAddon_Release]
 
 
-class TownlongYakApiResponse(TypedDict):
-    name: str
-    summary: str
-    author: str
-    pi: str  # Persistent project identifier used for updates
-    link: str  # Add-on home page
-    releases: list[TownlongYakApiResponse_Release]
+class WowUpHubAddon_Release(TypedDict):
+    body: str  # Changelog?  Includes HTML
+    download_count: int  # Always 0 for Townlong Yak
+    download_url: str
+    external_id: str  # Source ID
+    game_version: str  # TOC-style game version, e.g. "90002" for 9.0.2
+    id: int
+    name: str  # Cf. GH API
+    prerelease: bool
+    published_at: str
+    tag_name: str  # Cf. GH API
+    url: str  # Same as download URL for TY
+    game_type: Literal['retail', 'classic']
+
+
+class WowUpHubAddons(TypedDict):
+    addons: list[WowUpHubAddon]
 
 
 class TownlongYakResolver(Resolver):
     source = 'townlong-yak'
-    name = 'Townlong Yak'
+    name = 'Townlong Yak (via WowUp.Hub)'
     strategies = frozenset({Strategy.default})
 
-    # Reference: https://www.townlong-yak.com/addons/about/update-api
-    api_url = URL('https://www.townlong-yak.com/addons/api/install-bundle')
+    _api_url = 'https://hub.dev.wowup.io/addons/author/foxlit'
 
     @staticmethod
     def get_alias_from_url(value: str) -> str | None:
         url = URL(value)
-        if url.host == 'www.townlong-yak.com' and len(url.parts) == 3 and url.parts[1] == 'addons':
+        if url.host == 'www.townlong-yak.com' and len(url.parts) > 2 and url.parts[1] == 'addons':
             return url.parts[2]
+
+    async def _synchronise(self) -> dict[str, WowUpHubAddon]:
+        async with self.manager.locks['load Townlong Yak catalogue']:
+            foxlit_addons: WowUpHubAddons = await cache_response(
+                self.manager,
+                self._api_url,
+                {'minutes': 15},
+                label=f'Synchronising {self.name} catalogue',
+            )
+            return {URL(a['repository']).name: a for a in foxlit_addons['addons']}
 
     async def resolve_one(self, defn: Defn, metadata: None) -> Pkg:
         if defn.strategy not in self.strategies:
             raise PkgStrategyUnsupported(defn.strategy)
 
-        async with self.manager.web_client.get(self.api_url / defn.alias) as response:
-            if response.status == 404:
-                raise PkgNonexistent
+        addons = await self._synchronise()
+        try:
+            addon = addons[defn.alias]
+        except KeyError:
+            raise PkgNonexistent
 
-            api_metadata: TownlongYakApiResponse = await response.json()
-
-        release = next(
-            r for r in api_metadata['releases'] if r['ch'] == self.manager.config.is_classic
-        )
+        try:
+            file = next(
+                r
+                for r in addon['releases']
+                if not r['prerelease'] and r['game_type'] == self.manager.config.game_flavour
+            )
+        except StopIteration:
+            raise PkgFileUnavailable
 
         return Pkg(
             source=self.source,
             id=defn.alias,
             slug=defn.alias,
-            name=api_metadata['name'],
-            description=api_metadata['summary'],
-            url=api_metadata['link'],
-            download_url=release['dl'],
+            name=addon['repository_name'],
+            description=addon['description'],
+            url=addon['repository'],
+            download_url=file['download_url'],
             date_published=datetime.now(timezone.utc),
-            version=URL(release['dl']).name,
+            version=file['tag_name'],
             options=PkgOptions(strategy=defn.strategy),
         )
 
